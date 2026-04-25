9da352f1be8edd4af9bbda74c15cea14b4902333
Morgan <momiternique@gmail.com>
2026-04-25 13:52:51 +0200

    Fix: init DB avant les requêtes sur Vercel

--- api/index.js
+++ api/index.js
@@ -1,2 +1,11 @@
+import { initDb } from '../backend/database.js'
 import app from '../backend/server.js'
-export default app
+
+// Initialise la DB au démarrage de la fonction serverless
+let ready = false
+const init = initDb().then(() => { ready = true }).catch(console.error)
+
+export default async function handler(req, res) {
+  if (!ready) await init
+  return app(req, res)
+}
